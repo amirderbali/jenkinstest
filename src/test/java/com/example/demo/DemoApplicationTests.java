da277953b2c5dc73f1f8da8289c8ab517e08f2d0
Amir <amirderbali81@gmail.com>
2025-04-11 13:38:24 +0200

    salut

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+ 
 @SpringBootTest
 class DemoApplicationTests {
 
